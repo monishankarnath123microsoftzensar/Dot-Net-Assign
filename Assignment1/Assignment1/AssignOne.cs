dd540006c69aa88dbc3400cd3fc9204b0600ab29
monishankar nath <monishankar.nath@zensar.com>
2020-08-02 22:39:16 +0530

    Update of IsSquare

--- Assignment1/Assignment1/AssignOne.cs
+++ Assignment1/Assignment1/AssignOne.cs
@@ -9,18 +9,15 @@ namespace Assignment1
     abstract class Quadrilateral
     {
         public int length;
-        public Quadrilateral(int _length)
-        {
-            length = _length;
-        }
+        
         public abstract double Area();
     }
 
     class Square : Quadrilateral
     {
-        public Square(int _length):base(_length)
+        public Square(int _length)
         {
-                
+            length = _length;
         }
         public override double Area()
         {
@@ -30,28 +27,26 @@ namespace Assignment1
 
     class Rectangle : Quadrilateral
     {
-        int breadth;
-        
-        public int IsSquare
-        {
-           get { return breadth; }
-          set { 
-            if(value == length)
-                {
+        public int breadth;
 
-                    Console.WriteLine("True");
-                    
+        public bool IsSquare
+        {
+
+            get
+            {
+                if (breadth == length)
+                {
+                    return true;
+
                 }
-            else{
-                    breadth = value;
-                }
-                    }
+                return false;
+            }
         }
 
-        public Rectangle(int _length, int _breadth):base(_length)
+        public Rectangle(int _length, int _breadth)
         {
             breadth = _breadth;
-
+            length = _length;
                 
         }
         public override double Area()
@@ -64,10 +59,10 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Quadrilateral sq = new Square(4);
-            Quadrilateral re = new Rectangle(6, 6);
-            Console.WriteLine("Area SQ {0}",sq.Area());
-            Console.WriteLine("Area RE {0}", re.Area());
+            Quadrilateral sq = new Square(10);
+            Rectangle re = new Rectangle(5, 6);
+            //Console.WriteLine("Area of SQ {0}",sq.Area());
+            //Console.WriteLine("Area of RECT {0}", re.Area());
            
             if(sq.Area() > re.Area())
             {
@@ -76,6 +71,10 @@ namespace Assignment1
             else
             {
                 Console.WriteLine("Square area is smaller than Rectangle area");
+            }
+            if (re.IsSquare)
+            {
+                Console.WriteLine("Reactangle is a square for given length & breadth");
             }
         }
     }

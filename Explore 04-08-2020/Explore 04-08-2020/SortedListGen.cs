4483e1e1f3a2f9c2406a8fd4b0906ff4bb833920
monishankar nath <monishankar.nath@zensar.com>
2020-08-04 23:21:54 +0530

    Update sortedlist Demo

--- Explore 04-08-2020/Explore 04-08-2020/SortedListGen.cs
+++ Explore 04-08-2020/Explore 04-08-2020/SortedListGen.cs
@@ -12,18 +12,44 @@ namespace Explore_04_08_2020
         static void Main()
         {
 
-            SortedList<int, string> sl1 = new SortedList<int, string>();
-            sl1.Add(3, "Three");
-            sl1.Add(1, "One");
-            sl1.Add(2, "Two");
-            sl1.Add(4, null);
-            IList il = sl1.GetValueList();
-            Console.WriteLine("Values short listed");
-            foreach (var it in il)
+            SortedList<string, string> slist = new SortedList<string, string>();
+            slist.Add("India", "MSDhoni");
+            slist.Add("Australia", "RickyPonting");
+            slist.Add("England", "KevinPeterson");
+            slist.Add("Newzealand", "Williamson");
+            slist.Add("SouthAfrica", "ABDvillers");
+            Console.WriteLine("*******Remove******");
+            slist.Remove("England");
+            Console.WriteLine("list after removing");
+            foreach (var item in slist)
             {
-                Console.WriteLine(it);
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+            Console.WriteLine("********Index of key*******");
+            Console.WriteLine("Index of key india is :{0}", slist.IndexOfKey("India"));
+            Console.WriteLine("************Index of value**********");
+            Console.WriteLine("Index of Value is :{0}", slist.IndexOfValue("MSDhoni"));
+
+            Console.WriteLine("********containskey*********");
+            Console.WriteLine(slist.ContainsKey("India"));
+            Console.WriteLine("******containsvalue********");
+            Console.WriteLine(slist.ContainsValue("MSDhoni"));
+            Console.WriteLine("***********Get Value*********");
+            Console.WriteLine(slist.TryGetValue("India", out _));
+            Console.WriteLine("***********Remove at*********");
+            slist.RemoveAt(3);
+            Console.WriteLine("after removing 4th index");
+            foreach (var item in slist)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
             }
 
+            IEnumerator<KeyValuePair<string, string>> ie = slist.GetEnumerator();
+            Console.WriteLine("Display");
+            while (ie.MoveNext())
+            {
+                Console.WriteLine("{0} : {1}", ie.Current.Key, ie.Current.Value);
+            }
         }
     }
 }

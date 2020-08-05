ebd9c1ef4d4fc4c901f75aec71c71990f3136f2c
monishankar nath <monishankar.nath@zensar.com>
2020-08-05 09:49:40 +0530

    Person Update

--- Assignment 04-08-2020/Assignment 04-08-2020/Program.cs
+++ Assignment 04-08-2020/Assignment 04-08-2020/Program.cs
@@ -32,8 +32,31 @@ namespace Assignment_04_08_2020
         {
             static void Main(string[] args)
         {
-            List<string,string> LPerson = new List<Person>();
-            LPerson.
+            List<Person> LPerson = new List<Person>();
+            Person per1 = new Person(21, "Moni", "Nath", "Kolkata");
+            Person per2 = new Person(32, "Moni", "Nath", "Chennai");
+            Person per3 = new Person(51, "Dustu", "Nath", "goriwali");
+            Person per4 = new Person(27, "David", "Bhusan", "Pune");
+            Person per5 = new Person(29, "Shankar", "Nath", "Kolkata");
+            
+            LPerson.Add(per1);
+            LPerson.Add(per2);
+            LPerson.Add(per3);
+            LPerson.Add(per4);
+            LPerson.Add(per5);
+
+            //    var listper = new List<Person>
+            //    {
+            //        new Person(21, "Moni", "Nath", "Kolkata"),
+            //        new Person(32, "Moni", "Nath", "Chennai"),
+            //        new Person(51, "Moni", "Nath", "goriwali"),
+            //        new Person(27, "David", "Bhusan", "Pune"),
+            //        new Person(29, "Shankar", "Nath", "Kolkata"),
+            //};
+            foreach (Person p in LPerson)
+            {
+                p.DisplayPerson();
+            }
         }
     }
 }

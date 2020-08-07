d16b8e87e5f8d75db9b5e92416708a9e6d460f6a
monishankar nath <monishankar.nath@zensar.com>
2020-08-08 00:15:23 +0530

    Explore 07-08-2020

---
+++ Explore 07-08-2020/Explore 07-08-2020/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Explore_07_08_2020
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string path = @"E:\temp\MyTest.txt";
+
+            
+            if (!File.Exists(path))
+            {
+               
+                string createText = "Hello and Welcome";
+                File.WriteAllText(path, createText);
+            }
+
+            
+            string appendText = "This is extra text";
+            File.AppendAllText(path, appendText);
+
+            File.Copy(@"E:\temp\MyTest.txt", @"E:\temp\MyTest2.txt");
+            string readText = File.ReadAllText(path);
+
+            File.Delete(@"E:\temp\MyTest2.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
